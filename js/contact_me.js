76f8cdb78d9ca14a235c3c16f855bdd28ecd20d7
Jussi Virtanen <jussi@iwa.fi>
2018-07-31 15:28:50 +0300

    Add recaptcha to contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,13 @@
 $(function() {
 
+  showSendButton = function() {
+    $("#sendMessageButton").show();
+  };
+
+  hideSendButton = function() {
+    $("#sendMessageButton").hide();
+  };
+
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
@@ -12,6 +20,7 @@ $(function() {
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
+      var g_recaptcha_response = $("textarea#g-recaptcha-response").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -20,13 +29,14 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "/mail/contact_me",
         type: "POST",
         data: {
           name: name,
           phone: phone,
           email: email,
-          message: message
+          message: message,
+          'g-recaptcha-response': g_recaptcha_response
         },
         cache: false,
         success: function() {
@@ -35,7 +45,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Viestisi on lähetetty. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +56,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Pahoittelut " + firstName + ", näyttää siltä, että viestiä ei voida lähettää. Kokeile myöhemmin uudelleen tai soita vaikka minulle."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
