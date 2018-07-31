1c56d50e78c0274baade93b23e13cd6277edc5ba
Jussi Virtanen <jussi@iwa.fi>
2018-07-31 19:39:37 +0300

    Reset recaptcha after successfull submit

--- js/contact_me.js
+++ js/contact_me.js
@@ -40,6 +40,8 @@ $(function() {
         },
         cache: false,
         success: function() {
+          // Reset Google reCatpcha
+          grecaptcha.reset();
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
